d1451dc658a168acd97d19d8702ac65b114d6c5a
Li Dong <705723886@qq.com>
2022-10-31 21:50:55 +0800

    save

--- scripts/run-vsftpd.sh
+++ scripts/run-vsftpd.sh
@@ -1,59 +1,50 @@
 #!/bin/bash
 
-# If no env var for FTP_USER has been specified, use 'admin':
-if [ "$FTP_USER" = "**String**" ]; then
-    export FTP_USER='admin'
-fi
-
-# If no env var has been specified, generate a random password for FTP_USER:
-if [ "$FTP_PASS" = "**Random**" ]; then
-    export FTP_PASS=`cat /dev/urandom | tr -dc A-Z-a-z-0-9 | head -c${1:-16}`
-fi
-
-# Do not log to STDOUT by default:
-if [ "$LOG_STDOUT" = "**Boolean**" ]; then
-    export LOG_STDOUT=''
-else
-    export LOG_STDOUT='Yes.'
-fi
-
 # Create home dir and update vsftpd user db:
-mkdir -p "/home/vsftpd/${FTP_USER}"
-chown -R ftp:ftp /home/vsftpd/
+mkdir -p "/home/vsftpd/${FTP_USER}" "/var/ftp" "/app/config"
+chown -R ftp:ftp /home/vsftpd/ /var/ftp
 
-echo -e "${FTP_USER}\n${FTP_PASS}" > /etc/vsftpd/virtual_users.txt
-/usr/bin/db_load -T -t hash -f /etc/vsftpd/virtual_users.txt /etc/vsftpd/virtual_users.db
+echo -e "${FTP_USER}\n${FTP_PASS}" > /app/config/virtual_users.txt
+/usr/bin/db_load -T -t hash -f /app/config/virtual_users.txt /app/config/virtual_users.db
 
-# Set passive mode parameters:
-if [ "$PASV_ADDRESS" = "**IPv4**" ]; then
-    export PASV_ADDRESS=$(/sbin/ip route|awk '/default/ { print $3 }')
-fi
+cat > /app/config/vsftpd.conf << EOB
+background=NO
+anonymous_enable=NO
+local_enable=YES
+guest_enable=YES
+virtual_use_local_privs=YES
+write_enable=YES
+pam_service_name=vsftpd_virtual
+user_sub_token=\$USER
+local_root=/home/vsftpd/\$USER
+chroot_local_user=YES
+allow_writeable_chroot=YES
+hide_ids=YES
+xferlog_enable=YES
+xferlog_file=/var/log/vsftpd/vsftpd.log
+port_enable=YES
+connect_from_port_20=YES
+ftp_data_port=20
+seccomp_sandbox=NO
+EOB
 
-echo "pasv_address=${PASV_ADDRESS}" >> /etc/vsftpd/vsftpd.conf
-echo "pasv_max_port=${PASV_MAX_PORT}" >> /etc/vsftpd/vsftpd.conf
-echo "pasv_min_port=${PASV_MIN_PORT}" >> /etc/vsftpd/vsftpd.conf
-echo "pasv_addr_resolve=${PASV_ADDR_RESOLVE}" >> /etc/vsftpd/vsftpd.conf
-echo "pasv_enable=${PASV_ENABLE}" >> /etc/vsftpd/vsftpd.conf
-echo "file_open_mode=${FILE_OPEN_MODE}" >> /etc/vsftpd/vsftpd.conf
-echo "local_umask=${LOCAL_UMASK}" >> /etc/vsftpd/vsftpd.conf
-echo "xferlog_std_format=${XFERLOG_STD_FORMAT}" >> /etc/vsftpd/vsftpd.conf
-echo "reverse_lookup_enable=${REVERSE_LOOKUP_ENABLE}" >> /etc/vsftpd/vsftpd.conf
-echo "pasv_promiscuous=${PASV_PROMISCUOUS}" >> /etc/vsftpd/vsftpd.conf
-echo "port_promiscuous=${PORT_PROMISCUOUS}" >> /etc/vsftpd/vsftpd.conf
+echo "pasv_address=${PASV_ADDRESS}" >> /app/config/vsftpd.conf
+echo "pasv_max_port=${PASV_MAX_PORT}" >> /app/config/vsftpd.conf
+echo "pasv_min_port=${PASV_MIN_PORT}" >> /app/config/vsftpd.conf
+echo "pasv_addr_resolve=${PASV_ADDR_RESOLVE}" >> /app/config/vsftpd.conf
+echo "pasv_enable=${PASV_ENABLE}" >> /app/config/vsftpd.conf
+echo "file_open_mode=${FILE_OPEN_MODE}" >> /app/config/vsftpd.conf
+echo "local_umask=${LOCAL_UMASK}" >> /app/config/vsftpd.conf
+echo "xferlog_std_format=${XFERLOG_STD_FORMAT}" >> /app/config/vsftpd.conf
+echo "pasv_promiscuous=${PASV_PROMISCUOUS}" >> /app/config/vsftpd.conf
+echo "port_promiscuous=${PORT_PROMISCUOUS}" >> /app/config/vsftpd.conf
 
 # Get log file path
-export LOG_FILE=`grep xferlog_file /etc/vsftpd/vsftpd.conf|cut -d= -f2`
+export LOG_FILE=`grep xferlog_file /app/config/vsftpd.conf|cut -d= -f2`
 
 # stdout server info:
 if [ ! $LOG_STDOUT ]; then
 cat << EOB
-	*************************************************
-	*                                               *
-	*    Docker image: fauria/vsftpd                *
-	*    https://github.com/fauria/docker-vsftpd    *
-	*                                               *
-	*************************************************
-
 	SERVER SETTINGS
 	---------------
 	· FTP User: $FTP_USER
@@ -66,6 +57,4 @@ else
 fi
 
 # Run vsftpd:
-sleep 100000
-
-&>/dev/null /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
+# &>/dev/null /app/bin/vsftpd /app/config/vsftpd.conf
